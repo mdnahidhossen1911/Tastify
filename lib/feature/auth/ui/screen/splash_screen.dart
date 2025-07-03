a7f4ea727c6342ca64d849b804a4c358864e2b69
jahangirsim <jahangirsim@gmail.com>
2025-07-04 00:44:26 +0600

    Splash Screen updated

--- lib/feature/auth/ui/screen/splash_screen.dart
+++ lib/feature/auth/ui/screen/splash_screen.dart
@@ -26,8 +26,32 @@ class _SplashScreenState extends State<SplashScreen> {
             // Color.fromARGB(255, 255, 233, 1)
           ]),
         ),
-        child: Center(
-          child: Image.asset(AssetsPath.appLogoPNG, height: 60, width: 210),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 400,), // Need to discuss about centering then positioning Container
+            Center(
+              child: Image.asset(AssetsPath.appLogoPNG, height: 60, width: 210),
+
+            ),
+            Spacer(),
+            Container(
+              height: 20,
+              width: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Color.fromRGBO(255, 230, 230, 1),
+                      Color.fromRGBO(255, 255, 233, 1),
+                ])
+              ),
+            ),
+            SizedBox(height: 50,)
+          ],
         ),
       ),
     );
